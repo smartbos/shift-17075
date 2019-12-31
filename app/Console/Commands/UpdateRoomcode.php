655b8711b9ca9f897a5b727570adea34581c5309
Hyunseok Lee <smartbosslee@gmail.com>
2019-12-31 21:57:33 +0900

    2020년 룸코드 입력 커맨드

--- app/Console/Commands/UpdateRoomcode.php
+++ app/Console/Commands/UpdateRoomcode.php
@@ -39,8 +39,31 @@ class UpdateRoomcode extends Command
      */
     public function handle()
     {
-        $date = Carbon::createFromDate(2019, 12, 16);
-        $end = Carbon::createFromDate(2019, 12, 31);
+        $rooms = [
+            [
+                'branch_id' => 1,
+                'room_type' => '세미나실 3인실'
+            ],
+            [
+                'branch_id' => 1,
+                'room_type' => '세미나실 6인실'
+            ],
+            [
+                'branch_id' => 1,
+                'room_type' => '세미나실 8인실'
+            ],
+            [
+                'branch_id' => 2,
+                'room_type' => '세미나실 A'
+            ],
+            [
+                'branch_id' => 2,
+                'room_type' => '세미나실 B'
+            ],
+        ];
+
+        $date = Carbon::createFromDate(2020, 1, 1);
+        $end = Carbon::createFromDate(2020, 12, 31);
 
         for ($date; $date <= $end; $date->addDay()) {
             $code = implode("", [
@@ -54,10 +77,14 @@ class UpdateRoomcode extends Command
                 rand(0, 9),
             ]);
 
-            Roomcode::where('date', $date->format('Y-m-d'))
-                ->update([
-                    'code' => $code
+            foreach($rooms as $room) {
+                Roomcode::create([
+                    'date' => $date->format('Ymd'),
+                    'code' => $code,
+                    'room_type' => $room['room_type'],
+                    'branch_id' => $room['branch_id']
                 ]);
+            }
         }
     }
 }
